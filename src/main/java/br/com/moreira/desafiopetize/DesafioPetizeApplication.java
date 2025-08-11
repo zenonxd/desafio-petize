67276f169c47c3afab0b57823888dccd624d18b8
zenonxd <olavomoreira99@gmail.com>
2025-08-11 14:51:25 -0300

    feat: first commit

---
+++ src/main/java/br/com/moreira/desafiopetize/DesafioPetizeApplication.java
@@ -0,0 +1,13 @@
+package br.com.moreira.desafiopetize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPetizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPetizeApplication.class, args);
+    }
+
+}
