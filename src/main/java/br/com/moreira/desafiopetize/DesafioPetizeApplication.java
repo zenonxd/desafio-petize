7003273bd272dd6354c284ff27f56e0c53d81663
zenonxd <olavomoreira99@gmail.com>
2025-08-12 14:41:02 -0300

    feat: security config + fixing dependencies

--- src/main/java/br/com/moreira/desafiopetize/DesafioPetizeApplication.java
+++ src/main/java/br/com/moreira/desafiopetize/DesafioPetizeApplication.java
@@ -2,8 +2,10 @@ package br.com.moreira.desafiopetize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.com.moreira.desafiopetize.domain.repositories")
 public class DesafioPetizeApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/moreira/desafiopetize/config/SecurityConfig.java
+++ src/main/java/br/com/moreira/desafiopetize/config/SecurityConfig.java
@@ -1,23 +1,75 @@
 package br.com.moreira.desafiopetize.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${jwt.public.key}")
+    private RSAPublicKey key;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey priv;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/api/auth").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(
+                        conf -> conf.jwt(Customizer.withDefaults()));
 
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .build();
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // usando chave publica para decodificar o token jwt
+        return NimbusJwtDecoder.withPublicKey(key).build();
+
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        var jwk = new RSAKey.Builder(key).privateKey(priv).build();
+
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
